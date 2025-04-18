39b7668df35284d0a2d3280d5a9fc4ebfcc61442
Amankanojiya27 <amankanojiya815@gmail.com>
2025-04-18 12:23:06 +0530

    initialize

--- src/landing-page.tsx
+++ src/landing-page.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './globle.css'
 
 function App() {
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './globle.css'
 import App from './landing-page.tsx'
 
 createRoot(document.getElementById('root')!).render(
